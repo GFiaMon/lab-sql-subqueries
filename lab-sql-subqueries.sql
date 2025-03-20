00ae849d52c6a3f0530b24b8e0e11f3106188e4a
GFiaMon <chamigo@rocketmail.com>
2025-03-20 23:49:19 +0100

    Solved lab w/ Bonus

--- lab-sql-subqueries.sql
+++ lab-sql-subqueries.sql
@@ -95,14 +95,138 @@ WHERE
 -- --------------------------------------------------
 -- **Bonus**:
 -- --------------------------------------------------
--- 4. Sales have been lagging among young families, and you want to target family movies for a promotion. Identify all movies categorized as family films. 
+-- 4. Sales have been lagging among young families, and you want to target family movies for a promotion. 
+-- Identify all movies categorized as family films. 
+	-- Step 1
+Select * from film;
+Select * from film_category;
+Select * from category;
+
+	-- Step 2
+Select * from film
+join film_category
+using (film_id)
+join category
+using (category_id)
+where category.name = 'Family'
+;
+
+	-- Step 3 Convert to subquery
+Select *
+from film
+where film_id IN (
+	SELECT film_id
+    from film_category
+    join category using (category_id)
+    where category.name = 'Family'
+);
 
 
+-- 5. Retrieve the name and email of customers from Canada using both subqueries and joins. 
+-- To use joins, you will need to identify the relevant tables and their primary and foreign keys.
+	-- Step 1
+select * from customer;
+select * from address;
+select * from city;
+select * from country;
+	-- Step 2
+select country_id, country from country
+where country = 'canada'
+;
+	-- Step 3 FINAL
+SELECT 
+    customer.first_name, customer.last_name, customer.email
+FROM
+    customer
+JOIN address USING (address_id)
+JOIN city USING (city_id)
+JOIN country USING (country_id)
+WHERE
+    country.country_id = (SELECT 
+            country_id
+        FROM
+            country
+        WHERE
+            country = 'canada')
+;
 
--- 5. Retrieve the name and email of customers from Canada using both subqueries and joins. To use joins, you will need to identify the relevant tables and their primary and foreign keys.
+-- 6. Determine which films were starred by the most prolific actor in the Sakila database. 
+	-- A prolific actor is defined as the actor who has acted in the most number of films. 
+	-- First, you will need to find the most prolific actor and then use that actor_id to find the different films that he or she starred in.
 
--- 6. Determine which films were starred by the most prolific actor in the Sakila database. A prolific actor is defined as the actor who has acted in the most number of films. First, you will need to find the most prolific actor and then use that actor_id to find the different films that he or she starred in.
+	-- Step 1
+select * from film;
+select * from film_actor order by actor_id desc;
+select * from actor;
+	-- Step 2
+select actor_id, count(film_id) as film_amount from film_actor
+group by actor_id
+order by film_amount desc limit 1
+;
+	-- Step 3
+select film.title, first_name, last_name from actor
+join film_actor
+using (actor_id)
+join film
+using (film_id)
+where actor_id = (select actor_id from (select actor_id, count(film_id) as film_amount from film_actor
+group by actor_id
+order by film_amount desc limit 1)as sub)
+;
 
--- 7. Find the films rented by the most profitable customer in the Sakila database. You can use the customer and payment tables to find the most profitable customer, i.e., the customer who has made the largest sum of payments.
+
+-- 7. Find the films rented by the most profitable customer in the Sakila database. 
+-- You can use the customer and payment tables to find the most profitable customer, i.e., the customer who has made the largest sum of payments.
+	-- Step 1
+select * from payment;
+select * from customer;
+	-- Step 2
+select customer_id, sum(amount) as total_spent from customer
+join payment
+using (customer_id)
+group by customer_id
+order by total_spent desc
+limit 1
+;
+	-- Step 2
+select title, first_name, last_name from film
+join inventory
+using (film_id)
+join rental
+using (inventory_id)
+join customer
+using (customer_id)
+where customer_id = 
+(select customer_id from (select customer_id, sum(amount) as total_spent from customer
+join payment
+using (customer_id)
+group by customer_id
+order by total_spent desc
+limit 1)sub)
+;
 
 -- 8. Retrieve the client_id and the total_amount_spent of those clients who spent more than the average of the total_amount spent by each client. You can use subqueries to accomplish this.
+
+-- avg of the total_amount spent by each client
+select avg(total_client) as average_spent from (
+select customer_id, sum(amount) as total_client
+from payment
+group by customer_id) as clients_totals
+;
+
+-- client_id and the total_amount_spent of those clients who spent more than avg
+
+select customer_id, sum(amount) as total_amount_spent from payment
+group by customer_id
+having total_amount_spent > (select avg(total_client) as average_spent from (
+select customer_id, sum(amount) as total_client
+from payment
+group by customer_id) as clients_totals)
+order by total_amount_spent desc
+;
+
+
+
+
+
+
